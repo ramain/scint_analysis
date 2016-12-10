03e4ade1794f22b0bc0fa9b5988bdbb05019f76f
Robert Main <ramain@prawn.cita.utoronto.ca>
2016-12-09 19:00:37 -0500

    Fixed PulseFinder hang

--- reduction/PulseDetect.py
+++ reduction/PulseDetect.py
@@ -3,20 +3,26 @@ import numpy as np
 def PulseDetect(power, t, phase_pol, thresh=6, outfile='gp.txt'):
     """Flag peaks above S/N threshold"""
 
-    if power.shape == 3:
+    print("START")
+    if len(power.shape) == 3:
         power = power.sum(-1)
     freq_av = power.sum(-1)
+    print("SUMMED")
 
     sn = (freq_av-freq_av.mean()) / freq_av.std()
     # Loop to remove outliers from S/N calculation
+    print("LOOP DEALY")
     for i in xrange(3):        
         back = np.argwhere(sn < thresh)
         sn = (freq_av-freq_av[back].mean()) / freq_av[back].std()
 
+    print("ARGWHERE")
     peaks = np.argwhere(sn > thresh)
 
     f = open(outfile, 'a')
 
+    print(peaks)
+    print("WRITE DEALY")
     for peak in peaks:
         t_gp = t[peak]
         phase = np.remainder(phase_pol(t_gp.mjd), 1)
